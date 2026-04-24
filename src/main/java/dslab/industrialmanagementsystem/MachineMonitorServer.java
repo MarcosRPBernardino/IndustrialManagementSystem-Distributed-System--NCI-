9a26d15bbfbceb04e1ce4ed0afa6d2f52787f1de
Marcos Bernardino <marcospivatobernardino@gmail.com>
2026-04-24 02:36:32 +0100

    Integrated JmDNS registration into MachineMonitorServer

--- src/main/java/dslab/industrialmanagementsystem/MachineMonitorServer.java
+++ src/main/java/dslab/industrialmanagementsystem/MachineMonitorServer.java
@@ -13,12 +13,17 @@ import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
 import java.io.IOException;
 import java.util.Random;
+import java.net.InetAddress;
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceInfo;
 
 public class MachineMonitorServer extends MachineMonitorGrpc.MachineMonitorImplBase {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         MachineMonitorServer monitorServer = new MachineMonitorServer();
         int port = 40001;
+
+        monitorServer.registerService(port);
 
         Server server = ServerBuilder.forPort(port)
                 .addService(monitorServer)
@@ -28,6 +33,20 @@ public class MachineMonitorServer extends MachineMonitorGrpc.MachineMonitorImplB
         System.out.println("Machine Monitor Server started, listening on " + port);
 
         server.awaitTermination();
+    }
+
+    private void registerService(int port) {
+        try {
+            JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
+
+            ServiceInfo serviceInfo = ServiceInfo.create("_machine-monitor._tcp.local.", "machine-monitor", port, "Service for industrial monitoring");
+
+            jmdns.registerService(serviceInfo);
+            System.out.println("Service registered on JmDNS: " + serviceInfo.getName());
+
+        } catch (IOException e) {
+            System.out.println("Error registering service: " + e.getMessage());
+        }
     }
 
     @Override
@@ -55,7 +74,7 @@ public class MachineMonitorServer extends MachineMonitorGrpc.MachineMonitorImplB
         try {
             for (int i = 0; i < 10; i++) {
                 SensorData data = SensorData.newBuilder()
-                        .setTemperature(20 + (30 * random.nextDouble())) 
+                        .setTemperature(20 + (30 * random.nextDouble()))
                         .setPerformanceLoad(random.nextDouble() * 100)
                         .build();
 
